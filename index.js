1c2ac7085332e343e7615961b44b2f2b4c7c3d5e
Rafly Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-01-08 12:31:42 +0000

    CHORE: complete api test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,51 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/:date?', (req, res) => {
+  let dateParameter = req.params.date;
+
+  let date;
+  if (dateParameter) {
+    // If not a number alias ex: 20-20-2020
+    if (isNaN(dateParameter)) {
+      date = new Date(dateParameter)
+    } else {
+      date = new Date(parseInt(dateParameter))
+    }
+  }
+  else {
+    date = new Date();
+  }
+
+  let message = {};
+  if (date) {
+    console.log(date)
+    let unix = date.getTime()
+    console.log(unix)
+    let formattedDate = date.toUTCString()
+    console.log(formattedDate)
+    if (formattedDate == "Invalid Date") {
+      message = { error: "Invalid Date" }
+    }
+    else if (unix == null) {
+      message = { error: "Invalid Date" }
+    }
+    else {
+      message = {
+        unix: unix,
+        utc: formattedDate
+      }
+    }
+
+  }
+  else {
+    message = { error: "Invalid Date" }
+  }
+  res.json(message)
+})
 
 
 
